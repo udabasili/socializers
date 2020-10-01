662c8bdbb3fb90806553a749a0ffaa3be8f234f1
udabasili <udbasili@yahoo.com>
2020-09-30 22:22:16 -0300

    server update

--- app/index.js
+++ app/index.js
@@ -4,11 +4,12 @@ const xss = require("xss-clean");
 const helmet = require("helmet");
 const mongoSanitize = require("express-mongo-sanitize");
 const cloudinaryConfig = require("../utils/cloudinaryConfig")
-
+const cors = require('cors')
 //MIDDLEWARE PACKAGES
 //for security
 app.use(mongoSanitize());
 app.use(xss());
+app.use(cors())
 app.use(helmet())
 
 //others
